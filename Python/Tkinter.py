f9067f4e1920304c8e09613caf7397bc808c01d9
Sabir Mohammedi Taieb <52837091+Sabir97@users.noreply.github.com>
2020-05-29 10:11:56 +0100

    Python

---
+++ Python/Tkinter.py
@@ -0,0 +1,6 @@
+from tkinter import*
+from tkinter import ttk 
+rw = Tk()
+b1 = ttk.Button(rw,text="Click")
+b1.pack()
+b1.config(compound=RIGHT)
---
+++ Python/problem.py
@@ -0,0 +1,10 @@
+def test(n):
+    if(n==1): 
+     print("Bonjour") 
+    else:
+     if(n==2):
+      print("Bye")
+     else:
+        print("Hello!")
+
+test(3)
