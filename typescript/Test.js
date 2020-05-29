45f27adb408f92cb8d6cd095f366321f9ef72a4d
Sabir Mohammedi Taieb <52837091+Sabir97@users.noreply.github.com>
2020-05-29 10:09:39 +0100

    Trying Typescript

---
+++ typescript/Test.js
@@ -0,0 +1,6 @@
+//console.log("Hello World !");
+function fonc1(person) {
+    return "Hello " + person;
+}
+var user = "Sabir";
+console.log(fonc1(user));
---
+++ typescript/Test.ts
@@ -0,0 +1,9 @@
+//console.log("Hello World !");
+
+function fonc1(person){
+    return "Hello " + person;
+}
+
+let user = "Sabir";
+
+console.log(fonc1(user));
